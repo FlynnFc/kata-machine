d2c5ac6166dd2a6499cba0d823104e8b7a13e018
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-08-24 18:51:53 +0100

    lrus

--- src/day1/LRU.ts
+++ src/day1/LRU.ts
@@ -1,15 +1,93 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+    prev?: Node<T>;
+};
+
+function createNode<V>(value: V): Node<V> {
+    return { value };
+}
+
 export default class LRU<K, V> {
     private length: number;
+    private head?: Node<V>;
+    private tail?: Node<V>;
 
-    
+    private lookup: Map<K, Node<V>>;
+    private reverseLookup: Map<Node<V>, K>;
 
-    constructor() {
+    constructor(private capacity: number = 10) {
+        this.length = 0;
+        this.head = this.tail = undefined;
+        this.lookup = new Map<K, Node<V>>();
+        this.reverseLookup = new Map<Node<V>, K>();
     }
 
     update(key: K, value: V): void {
+        let node = this.lookup.get(key);
 
-}
+        if (!node) {
+            node = createNode(value);
+            this.length++;
+            this.prepend(node);
+            this.trimcache();
+            this.lookup.set(key, node);
+            this.reverseLookup.set(node, key);
+        } else {
+            node.value = value;
+            this.detach(node);
+            this.prepend(node);
+        }
+        // if it does update and put to front of the list
+    }
     get(key: K): V | undefined {
+        // Check if it exists
+        const node = this.lookup.get(key);
+        if (!node) {
+            return undefined;
+        }
+        // update the value, move it to the front
+        this.detach(node);
+        this.prepend(node);
+        // return out val found, or undefined
+        return node.value;
+    }
 
-}
+    private detach(node: Node<V>) {
+        if (node.prev) node.prev.next = node.next;
+        if (node.next) node.next.prev = node.prev;
+
+        if (this.head === node) {
+            this.head = this.head.next;
+        }
+        if (this.tail === node) {
+            this.tail = this.tail.prev;
+        }
+        node.next = undefined;
+        node.prev = undefined;
+    }
+    private prepend(node: Node<V>) {
+        if (!this.head) {
+            this.head = this.tail = node;
+            return;
+        }
+
+        node.next = this.head;
+        if (node.next) node.next.prev = node;
+        this.head = node;
+    }
+
+    private trimcache(): void {
+        if (this.length <= this.capacity) {
+            return;
+        }
+
+        const tail = this.tail as Node<V>;
+        this.detach(tail);
+
+        const key = this.reverseLookup.get(tail) as K;
+        this.lookup.delete(key);
+        this.reverseLookup.delete(tail);
+        this.length--;
+    }
 }
